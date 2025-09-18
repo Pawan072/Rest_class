b91154315abd8674853e78d1c8b45e2bbfadc962
Pawan072 <py8795116@gmail.com>
2025-09-18 19:50:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+app.use(express.urlencoded({extended: true}));
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname, "views"));
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+    {
+        username : "apna college",
+        content : "i love coding"
+    },
+    {
+        username : "shradha khapra",
+        content : "i got internship in microsoft in 1st attempet"
+    },
+    {
+        username : "Pawan Yadav",
+        content : "coding Student"
+    },
+];
+
+
+app.get("/posts",(req,res) => {
+    res.render("index.ejs" , {posts});
+});
+
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+});
+
+app.post("/posts",(req,res)=>{
+    let {username, content} = req.body;
+    posts.push({username,content});
+    res.redirect("/posts");
+});
+
+app.listen(port,()=>{
+    console.log(`server is running on port: ${port}`);
+});
